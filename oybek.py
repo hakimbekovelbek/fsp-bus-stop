c908e4b3dbc7854c98a101dbe64e93f058735e56
Abduqodirov Oybek <abduqodirovoybek44@gmail.com>
2022-05-13 19:31:43 +0500

    added oybek.py file

--- oybek.py
+++ oybek.py
@@ -1,2 +1,6 @@
 # Oybek
-# This is new line in oybek.py file
+# This is new line in oybek.py file.
+
+def calcLengthOfSlice(x1, y1, x2, y2):
+    c = (x2 - x1)**2 + (y2 - y1)**2
+    return c ** 0.5
