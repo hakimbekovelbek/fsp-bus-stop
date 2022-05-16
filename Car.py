2134c97004aeac6d88156433667f9b8bb8527a1b
asadbekz <assadzakkirov@gmail.com>
2022-05-16 20:55:37 +0500

    Changes

--- Car.py
+++ Car.py
@@ -1,20 +1,25 @@
 # макс. объем бака (л)
 # объем заполненности (л)
 # расход (л) за 100 км
-
 # ездить()
 # заполнить бензобак
 # показать сколько мы можем проехать км и остаток бензина
 
+
 from msilib.schema import Error
 
+fuel_type = {
+    80: 6500,
+    90: 7600,
+    91: 8000,
+    92: 9600
+}
 
 class Car:
     def __init__(self, volume, gas_left, consumption):
         self.volume = volume
         self.gas_left = gas_left
         self.consumption = consumption
-
     def ride(self, distance):
         try:
             if not isinstance(distance, (float, int)):
@@ -33,6 +38,7 @@ class Car:
                 print(err)
                 return err
 
+
     def fill_tank(self, added_fuel):
         if added_fuel < self.volume:
             self.gas_left = added_fuel
@@ -43,17 +49,16 @@ class Car:
                 print(err)
                 return err
 
+    def cal_gas_costs(self, type, litres):
+        pass
+
     def available(self):
         available_dist = (self.gas_left * 100) / self.consumption
-        return (self.gas_left, available_dist)
 
 matiz = Car(35, 25, 8)
-
-print(matiz.gas_left)
 matiz.ride(300)
 print(matiz.gas_left)
 matiz.ride(200)
+matiz.fill_tank()
 matiz.fill_tank(40)
 print(matiz.available())
-
-
