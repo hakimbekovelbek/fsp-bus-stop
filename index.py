70f632f720adec06341af7ad8d929f7950b3eb2f
Abduvali Valiyev <100288755+ritterzy@users.noreply.github.com>
2022-05-16 20:07:46 +0500

    added new func

--- index.py
+++ index.py
@@ -1,2 +1,31 @@
-def sum(a, b):
-    return a + b
+class Car:
+    def __init__(self,amount,oil,consumption,distance):
+        self.amount=amount
+        self.oil=oil
+        self.consumption=consumption
+        self.distance=distance
+    def fill_tank(self):
+
+        if self.amount >= self.oil:
+            return f'you filled to {self.amount-self.oil} L'
+        else:
+            self.amount <= self.oil
+            print("U can not fill your tank more than capacity")
+    def available_distance(self):
+        return f'your available distance is {self.oil} L for {self.oil / 0.15} km'
+    def ride(self):
+        time = self.distance / 70
+        consumed = self.distance * 0.15
+        balance=self.oil - consumed
+        return f'''
+        Time: {round(time,1)} Hrs
+        Consumed oil: {consumed}
+        Remaining fuel: {balance}'''
+        
+        
+        
+
+Lacetti =Car(75,55,15,100)
+# print(Lacetti.available_distance())
+print(Lacetti.fill_tank())
+print(Lacetti.ride())
