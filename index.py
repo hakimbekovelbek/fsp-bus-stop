0147eb99efdc0f4c26c68eb81a35f87f8c0ba62c
Abduvali Valiyev <100288755+ritterzy@users.noreply.github.com>
2022-05-16 20:39:14 +0500

    added function

--- index.py
+++ index.py
@@ -1,15 +1,23 @@
+gas_types={
+    80:6400,
+    92:9000,
+    95:10000
+}
 class Car:
     def __init__(self,amount,oil,consumption):
         self.amount=amount
         self.oil=oil
         self.consumption=consumption
-    def fill_tank(self):
 
-        if self.amount >= self.oil:
-            return f'you filled to {self.amount-self.oil} L'
+    def fill_tank(self,fuel_type):
+        if self.amount >= self.oil and fuel_type in gas_types:
+            filled=self.amount-self.oil
+            bill =filled * gas_types[fuel_type]
+            return f'you filled to {self.amount-self.oil} L and total cost is {bill} sums'
         else:
-            self.amount <= self.oil
-            print("U can not fill your tank more than capacity")
+            self.amount <= self.oil and fuel_type not in gas_types
+            print("U can not fill your tank more than capacity or you choosed incorrect type!")
+    
     def available_distance(self):
         return f'your available distance is {self.oil} L for {round(self.oil / 0.15,1)} km'
     def ride(self,distance):
@@ -20,11 +28,14 @@ class Car:
         Time: {round(time,1)} Hrs
         Consumed oil: {consumed}
         Remaining fuel: {balance}'''
+    def calc_gas_costs(self,type,litres):
+        pass
         
+
         
         
 
 Lacetti =Car(75,55,15)
-print(Lacetti.available_distance())
-print(Lacetti.fill_tank())
-print(Lacetti.ride(100))
+# print(Lacetti.available_distance())
+print(Lacetti.fill_tank(80))
+# print(Lacetti.ride(100))
