d6c844a1872adecbc44071de7cd5d82cbc9d584c
Ravshanov Khasan <ravshanovhasana@gmail.com>
2022-05-18 19:49:46 +0300

    added code which you exlain !!!

--- inheritance.py
+++ inheritance.py
@@ -1,25 +1,49 @@
-class Geometry:
-    def __init__(self, side_a, side_b):
-        self.side_a = side_a
-        self.side_b = side_b
+# class Geometry:
+#     def __init__(self, side_a, side_b):
+#         self.side_a = side_a
+#         self.side_b = side_b
 
-    def sqare(self):
-        a = self.side_a ** 2
-        b = (self.side_a + self.side_b) * 2
+#     def sqare(self):
+#         a = self.side_a ** 2
+#         b = (self.side_a + self.side_b) * 2
 
-        print('it is square - ', a)
-        print('it is perimetr - ', b)
+#         print('it is square - ', a)
+#         print('it is perimetr - ', b)
 
-        return
-    def rectangle(self):
-        a = self.side_a * self.side_b
-        c = (self.side_a + self.side_b) * 2
+#         return
+#     def rectangle(self):
+#         a = self.side_a * self.side_b
+#         c = (self.side_a + self.side_b) * 2
 
-        print('it is square - ', a)
-        print('it is perimetr - ', c)
+#         print('it is square - ', a)
+#         print('it is perimetr - ', c)
 
-        return
-f = Geometry(4, 5)  
-print(f.sqare())
-print(f.rectangle())
+#         return
+# f = Geometry(4, 5)  
+# print(f.sqare())
+# print(f.rectangle())
 
+class Parallelogram:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+
+class Rectangle(Parallelogram):
+    def __init__(self, a, b):
+        Parallelogram.__init__(self,a,b)
+
+class Rombus(Parallelogram):
+    def __init__(self, a):
+        self.a = a
+
+class Square(Parallelogram):
+    pass
+
+
+
+
+rect = Rectangle(2,3)
+print('rect',rect.a, rect.b)
+
+rombus = Rombus(5)
+print('rombus',rombus.a)
