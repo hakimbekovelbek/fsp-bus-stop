f8103db295ee8296675b103de29395f5631ed4de
Abduvali Valiyev <100288755+ritterzy@users.noreply.github.com>
2022-05-19 17:34:12 +0500

    class work

--- abduvali.py
+++ abduvali.py
@@ -1,10 +1,13 @@
 # This line is from Abduvali's laptop
 # This is new file in abduvali.py file
-
+import math
 class paralelogram:
-    def __init__(self,a_side,b_side):
+    def __init__(self,a_side,b_side,angle):
         self.a=a_side
         self.b=b_side
+        self.angle=angle
+    def area(self):
+        return self.a * self.b * math.sin(self.angle)
 class square(paralelogram):
     def __init__(self,a):
         self.a= a
@@ -16,6 +19,10 @@ class rectangular(paralelogram):
 class rhombus(paralelogram):
     def __init__(self,a):
         self.a=a
+
+class rectangle(paralelogram):
+    def __init__(self,a,b):
+        paralelogram.__init__
 
 parallelogram=paralelogram(5,8)
 Rhombus=rhombus(4)
