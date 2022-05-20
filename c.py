0a98b9cff2f5291dcd3bfb86febafea30451b3f1
Visola <visolad1@gmail.com>
2022-05-20 13:27:24 +0500

    hw

--- c.py
+++ c.py
@@ -1,22 +1,36 @@
+from math import *
+from tkinter import S
 class Parallelogram:
-    def __init__(self, a, b):
+    def __init__(self, a, b, angle):
         self.a = a
         self.b = b
+        self.angle = angle
 
+    def area(self):
+        return self.a * self.b * sin(radians(self.angle))
 
 class Square(Parallelogram):
     def __init__(self, a):
         self.a = a
+
+    def area(self):
+        return self.a ** 2 
+
         
 
 class Rectangle(Parallelogram):
     def __init__(self, a, b):
         Parallelogram.__init__(self, a, b)
 
+    def area(self):
+        return self.a * self.b
+
 
 class Rhombus(Parallelogram):
-    def __init__(self, a):
+    def __init__(self, a, angle):
         self.a = a
+        self.angle = angle
+    
+    def area(self):
+        return (self.a ** 2) * sin(radians(self.angle))
 
-par = Parallelogram(5, 6)
-sqr = Square(4)
