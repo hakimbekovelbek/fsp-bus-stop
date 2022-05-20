75213ad15fb63c19e9571f22139e7147103f1a2f
Iskandar Kasymov <iskandarkasymov12@gmail.com>
2022-05-20 15:53:17 +0500

    Added hometask

--- t_1.py
+++ t_1.py
@@ -1,18 +1,33 @@
 class Parallelogram:
-    def __init__(self , a , b):
+    def __init__(self , a , b , angle):
         self.a = a
         self.b = b
-class Square(Parallelogram):
+        self.angle = angle
     def area(self):
-        return self.a ** 2
+        import math 
+        return self.a * self.b * math.sin(math.radians(self.angle))
+        
 class Rectangle(Parallelogram):
+    def __init__(self , a , b ):
+        self.a = a
+        self.b = b
     def area(self):
         return self.a * self.b
 class Rhombus(Parallelogram):
+    def __init__(self, a , h):
+        self.a = a
+        self.h = h
+    def area(self):
+        return self.a * self.h
+class Square(Rhombus):
+    def __init__(self , a ):
+        self.a = a
     def area(self):
         return self.a ** 2
-pr = Parallelogram(3 , 2)
-sq = Square(3 , 3)
+pr = Parallelogram(10 , 10 , 90)
+sq = Square(3)
 rc = Rectangle(5 ,4)
-rh = Rhombus(1 , 1)
-print(sq.area() , rc.area() , rh.area())
+rh = Rhombus(10 , 3)
+objects = [pr , sq , rc , rh]
+for object in objects:
+    print(object.area())
