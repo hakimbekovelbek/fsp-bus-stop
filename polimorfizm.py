a687b801f1f5d72630d1dc7bf8f9fa037181c607
Ravshanov Khasan <ravshanovhasana@gmail.com>
2022-05-20 23:02:26 +0300

    added file third time

--- polimorfizm.py
+++ polimorfizm.py
@@ -10,24 +10,33 @@ class Star(Sky_object):
         self.color = color
 
 class Planet(Sky_object):
-    def __init__(self,name, type):
+    def __init__(self,name, type, mass):
         Sky_object.__init__(self, name)
         self.type = type
+        self._mass = mass
 
     def rotate(self):
         print(f'{self.name} is rotating ... ')
 
+    @property
+    def mass(self):
+        return self._mass
+    @mass.setter
+    def mass(self,new_mass):
+        if not(isinstance(new_mass, int)):
+            raise ValueError('Mass ought to be int')
+        self._mass = new_mass   
+    
 class Satelitte(Sky_object):
     def __init__(self,name):
         Sky_object.__init__(self,name)
         self.name = name
-
-    def get_mass(self):
-        pass
-
-class EarthPlsnet(Sky_object):
-    def __init__(self,name):
-        Sky_object.__init__(self,name)
+        
+earth = Planet('earth', 'solid', int(5.97e21))      
+print(earth.mass)
+# class EarthPlsnet(Sky_object):
+#     def __init__(self,name):
+#         Sky_object.__init__(self,name)
 
 
 
