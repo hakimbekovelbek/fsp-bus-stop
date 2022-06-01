9d4ca7363b87958ab11d6446207a93a84fa823a3
Ravshanov Khasan <ravshanovhasana@gmail.com>
2022-06-01 19:49:28 +0300

    added code

---
+++ bus_time/bus_time.py
@@ -0,0 +1,17 @@
+# import datetime
+# a = datetime.datetime.now()
+# b = (a.strftime("%A"))
+
+
+# def bus_stop():
+#     location = (input('enter a location - '))
+
+#     if location == A:
+
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.year)
+# print(x.strftime("%A"))
