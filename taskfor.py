4bdb6526145f23caacd012becf369781cce03d35
Ravshanov Khasan <ravshanovhasana@gmail.com>
2022-06-01 17:22:52 +0300

    aaa

--- taskfor.py
+++
@@ -1,9 +0,0 @@
-while True:
-    number = [int(input('enter a number - '))]
-    a = []
-    for i in number:
-        if len(number) == 0 :
-            print('error') 
-        else:
-            print(i + i)
-            
