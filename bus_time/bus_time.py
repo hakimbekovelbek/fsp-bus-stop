04d38929678741c7c6a57f7c97319d0f08a1762a
Ravshanov Khasan <ravshanovhasana@gmail.com>
2022-06-03 15:07:34 +0300

    added some code

--- bus_time/bus_time.py
+++ bus_time/bus_time.py
@@ -1,17 +1,103 @@
-# import datetime
-# a = datetime.datetime.now()
-# b = (a.strftime("%A"))
 
 
-# def bus_stop():
-#     location = (input('enter a location - '))
 
-#     if location == A:
+# ?   beruniy -- A
+# ?               tinchli --B
+# ?                           chorsu --C
+# ?                                      abdulla_qodiriy --D
+
+
+
+
+
+
 
 
 import datetime
+a = datetime.datetime.now()
+z = int(a.strftime('%H'))
 
-x = datetime.datetime.now()
 
-print(x.year)
-# print(x.strftime("%A"))
+hour = datetime.datetime.now()
+hrs = int(hour.strftime('%H'))
+
+minute = datetime.datetime.now()
+min = int(minute.strftime('%M'))
+
+gt = int((hrs * 60) + min)
+
+
+def bus_stop():
+
+    location = input('enter a location - ')
+
+    if location == 'beruniy':
+
+        if z > 1:
+            if gt > 60:
+                o = 14 - ((gt - (gt // 10) * 10 ))
+                print(f'{"104"}, destination "tinchli" {o}')
+                t = (gt - ((gt // 10) * 10))
+                print(f'{"434"} destination "beruniu" {t}')
+            
+            else:
+                print('blin')
+
+        else:
+            print('fuck off')
+
+    if location == 'tinchli':
+
+        if z > 1:
+            if gt > 60:
+                o = 14 - (gt - ((gt // 10) * 10))
+                print(f'{"104"} destination "tinchli" {o}')
+                t = (gt - ((gt // 10) * 10))
+                print(f'{"434"} destination "beruniy" {t}')
+            else:
+                print('blin')
+        else:
+            print('fuck off')
+
+    if location == 'chorsu':
+
+        if z > 1:
+            if gt > 60:
+                o = 28 - (gt - ((gt // 10) * 10))
+                print(f'{"104"} destination "chorsu" {o}')
+                t = 14 -(gt - ((gt // 10) * 10))
+                print(f'{"104"} destination "abdulla_qodiriy" {t}')
+                nl = 28 - (gt - ((gt // 10) * 10))
+                print(f'{"434"} destination "chorsu" {nl}')
+                nl = (gt - ((gt // 10) * 10))
+                print(f'{"434"} destination  "beruniy" {nl}')
+            else:
+                print('blin')
+        else:
+            print('fuck off')
+
+    if location == 'beruniy':
+        if z > 1:
+            if gt > 60:
+                o = 28 - (gt - (gt // 10) * 10)
+                print(f'{"104"} destination  "chorsu" {o}')
+                t = 14 - (gt - ((gt // 10) * 10))
+                print(f'{"104"} destination "abdulla_qodiriy" {t}')
+            else:
+                print('blin')
+        else:
+            print('fuck off')
+    if location == 'abdulla_qodiriy':
+        if z > 1:
+            if gt > 60:
+                o = 28 - (gt - (gt // 10) * 10)
+                print(f'{"104"} destination "abdulla_qodiriy" {o}')
+                t = 14 - (gt - (gt // 10) * 10)
+                print(f'{"104"} destination "chorsu" {t}')
+
+            else:
+                print('blin')
+        else:
+            print('fuck off')
+bus_stop()
+
