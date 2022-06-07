7be65f646648100c18608a19e17da92df70fda24
Umarjon Alimdjanov <ualimdjanov10@gmail.com>
2022-06-07 16:13:55 +0500

    added homework

--- umarjon.py
+++ umarjon.py
@@ -1,161 +1,109 @@
-# This line from Umarjon's laptop
-# This is test message
 
-fuel_types = {
-    80 : 6400,
-    92 : 9000,
-    95 : 1000,
-    100 : 1100
-}
-class Car:
-    def __init__(self, volume, filled, consuption):
-        self.volume = volume
-        self.filled = filled
-        self.consuption = consuption
+import datetime
+time = datetime.datetime.now()
 
-    def fill_tank(self, fuel_volume, fuel_type):
-        try:
-            if not(isinstance(fuel_volume, (float, int))) or not(isinstance(fuel_volume, (float, int))):
-                raise ValueError("Enter a number")
-        except ValueError as err:
-            print(err)
-            return err
-        if self.filled + fuel_volume < self.volume and fuel_type in fuel_types:
-            self.filled += fuel_volume
-            cost = fuel_volume * fuel_types[fuel_type]
-            print(f'It costs {cost} sums')
-            return cost
+def cur_time(datetime):
+    hours = int(datetime.strftime("%H"))
+    mins = int(datetime.strftime('%M'))
+    if hours == 0 or hours == 1:    
+        return (hours + 24) * 60 + mins
+    else:
+        return hours * 60 + mins
+
+
+def get_stations(file):
+    stations = {}
+    with open(file, 'r', encoding='utf-8') as rf:
+        while True:
+            line = rf.readline().strip().split(';')
+            if len(line) < 2:
+                break
+            stations.update({line[0] : int(line[1])})
+        return stations
+
+
+
+def bus_arrive(station, cur_time, stations):
+    print(f'Now is {time.strftime("%H")} : {time.strftime("%M")} ')
+    interval_81 = 15
+    interval_93 = 10
+    if 1500 > cur_time > 60:
+        if station == "E":
+            arrive_time = interval_81 - (cur_time % interval_81)
+            print(f'81, destination "F", {arrive_time} min')
+
+        elif station == "F":
+            arrive_time = interval_81 - (cur_time % interval_81)
+            print(f'81, destination "E", {arrive_time} min')
+        
+        elif station == "A":
+            spend_time_to_E = stations['F'] + stations['A']
+            spend_time_to_F = stations['E']
+            arrive_time = interval_93 - (cur_time % interval_93)
+            print(f'93, destination "D", {arrive_time} min')
+            arrive_time = interval_81 - (cur_time % interval_81) + spend_time_to_E
+            if arrive_time >= interval_81:
+                arrive_time -= interval_81
+            print(f'81, destination "E", {arrive_time} min')
+            arrive_time = interval_81 - (cur_time % interval_81) + spend_time_to_F
+            if arrive_time >= interval_81:
+                arrive_time -= interval_81
+            print(f'81, destination "F", {arrive_time} min')
+        
+        elif station == "B":
+            spend_time_to_E = stations['F']
+            spend_time_to_F = stations['E'] + stations['A']
+            spend_time_to_A = stations['C'] + stations['B']
+            spend_time_to_D = stations['A']
+            arrive_time = interval_93 - (cur_time % interval_93) + spend_time_to_D
+            if arrive_time >= interval_93:
+                arrive_time -= interval_93
+            print(f'93, destination "D", {arrive_time} min')
+            arrive_time = interval_93 - (cur_time % interval_93) + spend_time_to_A
+            if arrive_time >= interval_93:
+                arrive_time -= interval_93
+            print(f'93, destination "A", {arrive_time} min')
+            arrive_time = interval_81 - (cur_time % interval_81) + spend_time_to_E
+            if arrive_time >= interval_81:
+                arrive_time -= interval_81
+            print(f'81, destination "E", {arrive_time} min')
+            arrive_time = interval_81 - (cur_time % interval_81) + spend_time_to_F
+            if arrive_time >= interval_81:
+                arrive_time -= interval_81
+            print(f'81, destination "F", {arrive_time} min')
+        
+        elif station == "C":
+            spend_time_to_A = stations['D']
+            spend_time_to_D = stations['A'] + stations['B']
+            arrive_time = interval_93 - (cur_time % interval_93) + spend_time_to_A
+            if arrive_time >= interval_93:
+                arrive_time -= interval_93
+            print(f'93, destination "A", {arrive_time} min')
+            arrive_time = interval_93 - (cur_time % interval_93) + spend_time_to_D
+            if arrive_time >= interval_93:
+                arrive_time -= interval_93
+            print(f'93, destination "D", {arrive_time} min')
+
+        elif station == "D":
+            arrive_time = interval_93 - (cur_time % 10)
+            print(f'93, destination "A", {arrive_time} min')
+        
         else:
-            try:
-                if self.filled + fuel_volume > self.volume:
-                    raise ValueError('Fuel volume over than tank volume')
-                else: raise TypeError('Please select correct type of fuel')
-            except ValueError as err:
-                print(err)
-                return err
-            except TypeError as err:
-                print(err)
-                return err
-
-    def available_distance(self):
-        av_distance = round(self.filled / self.consuption * 100)
-        print(f'{round(self.filled)}, {av_distance} km')
-
-    def ride(self, distance):
-        if self.filled - (distance / 100 * self.consuption) < 0:
-            print('You cannot ride, because you need more fuel')
-        else: self.filled -= distance / 100 * self.consuption
-        
-        
-
-# lacetti = Car(75, 55, 15)
-# lacetti.fill_tank(10)
-# lacetti.available_distance()
-# lacetti.ride(100)
-# # lacetti.available_distance()
-# phantom = Car(150, 60, 40)
-# phantom.available_distance()
-# money_to_pay = phantom.fill_tank(80, 92)
-# phantom.ride(200)
-# phantom.available_distance()
-
-class Person:
-    def __init__(self, money):
-        self.money = money
-
-    def pay_money(self, amount):
-        if amount > self.money:
-            print("You don't have much money")
-        else: 
-            self.money -= amount
-
-# iskandar = Person(300_000)
-# iskandar.pay_money(money_to_pay)
-
-
-
-
-class Item:
-
-    def __init__(self, title, price):
-        self.title = title
-        self.price = price
-
-
-class Cart:
-    items = []
-    # def __init__(self):
-    
-    def add_item(self, item):
-        self.items.append(item)
-    
-    def remove_item(self, title, amount = 1):
-        try:
-            for i in range(amount):
-                self.items.remove(title)
-        except:
-            print('Enter correct title or amount')
+            print('Please enter correct station!')
             return
-    def show_items(self):
-        for item in self.items:
-            print("Name: " + item.title,"Price: " + str(item.price))
 
-    def total_price(self):
-        total = 0
-        for item in self.items:
-            total += item.price
-        print(f'Total cost: {total} UZS')
-        return total
-
-milk = Item('Milk', 11000)
-meat = Item('Beef', 70000)
-pepsi = Item('Pepsi', 10000)
-red_bull = Item('Red Bull', 25000)
-
-# cart = Cart()
-# cart.add_item(milk)
-# cart.add_item(meat)
-# cart.add_item(red_bull)
-# cart.add_item(pepsi)
-# cart.show_items()
-# cart.remove_item(pepsi)
-# cart.show_items()
-# cart.total_price()
+    else:
+        print("Buses are not available now!")
+        return
 
 
-
-import math
-
-class Parallellogram:
-    def __init__(self, a, b, angle):
-        self.a = a
-        self.b = b
-        self.angle = angle
-
-    def area(self):
-        return self.a * self.b * math.sin(math.radians(self.angle))
-
-class Rectangle(Parallellogram):
-    def __init__(self, a, b):
-        Parallellogram.__init__(self, a, b)
-
-    def area(self):
-        return self.a * self.b
-
-class Rhombus(Parallellogram):
-    def __init__(self, a, angle):
-        self.a = a
-        self.angle = angle
-
-    def area(self):
-        return self.a ** 2 * math.sin(math.radians(self.angle))
-
-class Square(Rhombus):
-    def __init__(self, a):
-        self.a = a
-
-    def area(self):
-        return self.a ** 2
-
-p1 = Parallellogram(10, 10, 90)
-print(p1.area())
+def start_programm():
+    stations = get_stations('stations.txt')
+    while True:
+        station = input("Enter your station:")
+        current_time = cur_time(time)
+        if station in stations.keys():
+            bus_arrive(station, current_time, stations)
+        else:
+            print('Please enter correct station')
+start_programm()
