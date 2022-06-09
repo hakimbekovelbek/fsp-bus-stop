5edf558291a9c873bdba11d504529eeb5f0d7b72
farangis djumanbaeva <shdjumanbaeva02@gmail.com>
2022-06-09 13:55:37 +0500

    bus stop added

--- python/bus.py
+++ python/bus.py
@@ -18,13 +18,44 @@ def bus_stop():
     if loc == 'beruniy':
         if z > 1:
             if gt>60:
-                o = 24 - ((gt - (gt//10) * 10 ))
+                o = 29 - ((gt - (gt//10) * 10 ))
                 print((f'{"104"}, destination "beruniy" {o}'))
-                t = ((gt - (gt//10) * 10 ))
+                t =  gt - (gt//10) * 10 
                 print((f'{"434"}, destination "beruniy" {o}'))
             else:
                 print('nothing found')
         else:
             print('nothing')
 
-    
+    elif loc == 'buyuk ipak yoli':
+            
+            if z > 1:
+                if gt>60:
+                    o = 26 - ((gt - (gt//10) * 10 ))
+                    print((f'{"1"}, destination "beruniy" {o}'))
+                    t = ((gt - (gt//10) * 10 ))
+                    print((f'{"58"}, destination "beruniy" {o}'))
+                else:
+                    print('nothing found')
+    elif loc == 'alisher navoiy':
+            
+        if z > 1:
+            if gt>60:
+                o = 23 - ((gt - (gt//10) * 10 ))
+                print((f'{"84"}, destination "beruniy" {o}'))
+                t = ((gt - (gt//10) * 10 ))
+                print((f'{"48"}, destination "beruniy" {o}'))
+            else:
+                print('nothing found')
+    elif loc == 'ozbekiston':
+            
+        if z > 1:
+            if gt>60:
+                o = 20 - ((gt - (gt//10) * 10 ))
+                print((f'{"95"}, destination "beruniy" {o}'))
+                t = ((gt - (gt//10) * 10 ))
+                print((f'{"15"}, destination "beruniy" {o}'))
+            else:
+                print('nothing found')
+
+bus_stop()
